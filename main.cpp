c7c946aa864c6b1ecb17a3fe4c765525e9539f16
mohammad hossein <mohammad.hajiebrahimi83@gmail.com>
2024-01-04 12:00:56 +0330

    add signup login logout

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define sep ' '
 #define endl '\n'
 #define tab '\t'
-
+typedef vector<pair<string,string>> VPSS;
 const string OK = "OK";
 const string EMPTY = "Empty";
 const string NOT_FOUND = "Not Found";
@@ -21,6 +21,9 @@ public:
     virtual string get_username(){return username;}
     virtual string get_password(){return password;}
     virtual string get_mode(){return mode;}
+    virtual void set_username(string user){username = user;}
+    virtual void set_password(string pass){password = pass;}
+    virtual void set_mode(string _mode){mode = _mode;}
 protected:
     string username;
     string password;
@@ -45,9 +48,70 @@ public:
 Artist::Artist():Client(){
     mode = ARTIST;
 }
+VPSS get_arg(int n){
+    VPSS content;
+    for (int i=0;i<n;i++){
+        string fir,sec,tmp=" ";
+        cin>>fir;
+
+        while(tmp[tmp.size()-1]!='>'){
+            cin>>tmp;
+            sec+=(" "+tmp);
+        }
+        sec = sec.substr(2,sec.length()-3);
+        content.push_back(make_pair(fir,sec));
+    }
+    return content;
+}
+Client* find_cli(string username , vector<Client*> clients){
+    for (int j=0;j<clients.size();j++){
+        if(clients[j]->get_username() == username) {
+            return clients[j];
+        }
+    }
+    return NULL;
+}
+Client* check_signup_exeption(Client* login_user, vector<Client*> clients, VPSS arg){
+    Client* new_cli = new Client();
+    for(int i=0;i<arg.size();i++){
+        if(arg[i].first == "username"){
+            for(int j=0;j<clients.size();j++){
+                if(clients[i]->get_username() == arg[i].second) throw string("invalid");
+            }
+            new_cli->set_username(arg[i].second);
+        }
+        if(arg[i].first == "mode" && (arg[i].second != "artist" && arg[i].second != "user")) throw string("invalid");
+        if(arg[i].first == "mode"){new_cli->set_mode(arg[i].second);}
+        if(arg[i].first == "password"){new_cli->set_password(arg[i].second);}
+    }
+    if (login_user!=NULL) throw string("Permission");
+    return new_cli;
+}
+Client* check_login_exeption(Client* login_user, vector<Client*> clients, VPSS arg){
+    Client* try_to_login =NULL;
+    for (int i=0;i<arg.size();i++){
+        if(arg[i].first == "username"){
+
+            try_to_login = find_cli(arg[i].second,clients);
+        }
+    }
+    if (try_to_login == NULL) throw string("Not exist");
+    for (int i=0;i<arg.size();i++){
+        if(arg[i].first == "password"){
+            if(arg[i].second != try_to_login->get_password()) throw string("Permission");
+        }
+    }
+    if (login_user!=NULL)throw string("Permission");
+    return try_to_login;
+}
+Client* check_logout_exeption(Client* login_user){
+    if(login_user ==NULL)throw string("Permission");
+    return NULL;
+}
 class Sputify{
 public:
     Sputify();
+    void commands();
 private:
     Client* login_user;
     vector<Client*> clients;
@@ -55,7 +119,50 @@ private:
 Sputify::Sputify(){
     login_user = NULL;
 }
-int main(){
+void Sputify::commands(){
+    string command;
+    while(cin>>command){
+        if (command == COMMANDS[1]){
+            string task,delimiter;
+            cin>>task>>delimiter;
+            if (task == "signup"){
+                VPSS arg = get_arg(3);
+                try{
+                    clients.push_back(check_signup_exeption(login_user,clients,arg));
+                    login_user = clients[clients.size()-1];
+                    cout<<OK<<endl;
+                }
+                catch (string err){
+                    if(err == "invalid"){cout<<BAD_REQUEST<<endl;}
+                    if(err == "Permission"){cout<<PERMISSION<<endl;}
 
+                }
+            }
+            if (task == "login"){
+                VPSS arg = get_arg(2);
+                try{
+                    login_user = check_login_exeption(login_user,clients,arg);
+                    cout<<OK<<endl;
+                }
+                catch(string err){
+                    if(err == "Not exist"){cout<<NOT_FOUND<<endl;}
+                    if(err == "Permission"){cout<<PERMISSION<<endl;}
+                }
+            }
+            if (task == "logout"){
+                try{
+                    login_user = check_logout_exeption(login_user);
+                    cout<<OK<<endl;
+                }
+                catch(string err){
+                    if(err == "Permission"){cout<<PERMISSION<<endl;}
+                }
+            }
+        }
+    }
+}
+int main(){
+    Sputify sputify;
+    sputify.commands();
     return 0;
 }
